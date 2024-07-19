1c8830fafabce5934079033d61d678fd436a09bc
[Jameel Ali] <[m.jameel.ali04@gmail.com]>
2024-07-19 15:55:27 +0100

    [add] functionality to move objects

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -2,46 +2,46 @@ import java.awt.*;
 import java.awt.event.*;        // Keeps track of mouse motion/clicks
 import javax.swing.*;
 
-public class UserInterface extends JPanel implements MouseListener, MouseMotionListener{
-    public void paintComponent(Graphics g){
+class UserInterface extends JPanel implements MouseListener, MouseMotionListener {
+    static int x = 0, y = 0;  // Global variables
+
+    public UserInterface() {
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         this.setBackground(Color.YELLOW);
 
-        this.addMouseListener(this);
-        this.addMouseMotionListener(this);
-
         g.setColor(Color.BLUE);
-        g.fillRect(20, 20, 20, 20);
-        g.setColor(new Color(190,0,190));
+        g.fillRect(x - 20, y - 20, 40, 40);
+
+        g.setColor(new Color(190, 0, 190));
         g.fillRect(40, 20, 80, 50);
     }
 
-    public void mouseMoved(MouseEvent e){
-        
+    public void mouseMoved(MouseEvent e) {
     }
 
-    public void mousePressed(MouseEvent e){
-        
+    public void mousePressed(MouseEvent e) {
     }
 
-    public void mouseReleased(MouseEvent e){
-        
+    public void mouseReleased(MouseEvent e) {
     }
 
-    public void mouseClicked(MouseEvent e){
-        
+    public void mouseClicked(MouseEvent e) {
     }
 
-    public void mouseDragged(MouseEvent e){
-        
+    public void mouseDragged(MouseEvent e) {
+        x = e.getX();
+        y = e.getY();
+        repaint();  // Redraws
     }
 
-    public void mouseEntered(MouseEvent e){
-        
+    public void mouseEntered(MouseEvent e) {
     }
 
-    public void mouseExited(MouseEvent e){
-        
+    public void mouseExited(MouseEvent e) {
     }
-
-}   
+}
