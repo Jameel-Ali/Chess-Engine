b3df55f804d94652fe732f017e7b32a7b4158973
[Jameel Ali] <[m.jameel.ali04@gmail.com]>
2024-07-19 16:49:36 +0100

    [add] ability for single pawn to be moved

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,13 +1,37 @@
 import java.awt.*;
-import java.awt.event.*;        // Keeps track of mouse motion/clicks
+import java.awt.event.*; // Keeps track of mouse motion/clicks
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 class UserInterface extends JPanel implements MouseListener, MouseMotionListener {
-    static int x = 0, y = 0;  // Global variables
+    static int x = 0, y = 0; // Global variables
+    private Image chessPieceImage; // Image variable
 
     public UserInterface() {
         this.addMouseListener(this);
         this.addMouseMotionListener(this);
+        loadChessPieceImage(); // Load the image once
+    }
+
+    private void loadChessPieceImage() {
+        try {
+            File imageFile = new File("../ChessPieces.png"); 
+            chessPieceImage = ImageIO.read(imageFile);
+            if (chessPieceImage == null) {
+                System.out.println("Image not loaded properly!");
+            } else {
+                System.out.println("Image loaded successfully!");
+                System.out.println("Image dimensions: " + chessPieceImage.getWidth(this) + "x" + chessPieceImage.getHeight(this));
+            }
+        } catch (IOException e) {
+            System.out.println("Image not found or unable to load!");
+            e.printStackTrace();
+        }
+
+        // Print the current working directory for debugging purposes
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
     }
 
     public void paintComponent(Graphics g) {
@@ -21,6 +45,13 @@ class UserInterface extends JPanel implements MouseListener, MouseMotionListener
         g.fillRect(40, 20, 80, 50);
 
         g.drawString("Hello World", x, y);
+
+        // Draw the image if it's loaded
+        if (chessPieceImage != null) {
+            g.drawImage(chessPieceImage, x, y, x+64, y+64, 0, 0, 64, 64, this);
+        } else {
+            System.out.println("chessPieceImage is null");
+        }
     }
 
     public void mouseMoved(MouseEvent e) {
@@ -38,7 +69,7 @@ class UserInterface extends JPanel implements MouseListener, MouseMotionListener
     public void mouseDragged(MouseEvent e) {
         x = e.getX();
         y = e.getY();
-        repaint();  // Redraws
+        repaint(); // Redraws
     }
 
     public void mouseEntered(MouseEvent e) {
