c92e42e75bae32778f510369b5040d109f5b3833
[Jameel Ali] <[m.jameel.ali04@gmail.com]>
2024-07-19 20:47:00 +0100

    [add] functionality for Queen

--- src/AlphaBetaChess.java
+++ src/AlphaBetaChess.java
@@ -96,8 +96,48 @@ public class AlphaBetaChess {
     }
 
     public static String possibleQ(int i) {
-        String list = "";
-        // Implement queen move logic here
+        String list = "", oldPiece;
+
+        int r = i / 8, c = i % 8;
+
+        int temp = 1;
+        for (int j = -1 ; j <= 1 ; j++){
+            
+            for (int k = -1 ; k <= 1 ; k++){
+                try {
+                    while (" ".equals(chessBoard[r+temp*j][c+temp*k]) ) {
+                        oldPiece = chessBoard[r+temp*j][c+temp*k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r+temp*j][c+temp*k] = "Q";
+
+                        if (kingSafe()){
+                            list = list + r + c + (r+temp*j) + (c+temp*k) + oldPiece;
+                        }
+
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r+temp*j][c+temp*k] = oldPiece;
+                        temp++;
+                    }
+
+                    if (Character.isLowerCase(chessBoard[r+temp*j][c+temp*k].charAt(0))){
+                        oldPiece = chessBoard[r+temp*j][c+temp*k];
+                        chessBoard[r][c] = " ";
+                        chessBoard[r+temp*j][c+temp*k] = "Q";
+
+                        if (kingSafe()){
+                            list = list + r + c + (r+temp*j) + (c+temp*k) + oldPiece;
+                        }
+
+                        chessBoard[r][c] = "Q";
+                        chessBoard[r+temp*j][c+temp*k] = oldPiece;
+                    }
+
+                } catch (Exception e) {}
+
+                temp = 1;
+            }    
+        }
+        
         return list;
     }
 
