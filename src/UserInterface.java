9725c34f1d6d25451415767be06fb7f0e7909e1c
[Jameel Ali] <[m.jameel.ali04@gmail.com]>
2024-07-19 15:30:41 +0100

    [fix] UI components not appearing on window

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 
 public class UserInterface extends JPanel{
     public void paintComponent(Graphics g){
-        
+        g.setColor(Color.BLUE);
+        g.fillRect(20, 20, 20, 20);
+        g.setColor(new Color(190,0,190));
+        g.fillRect(40, 20, 80, 50);
     }
 }   
